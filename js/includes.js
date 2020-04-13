8441899ce5b31cd384b1cc97de85c7e1e481895d
Elliott Cost <elliottcost@gmail.com>
2020-04-13 01:10:39 -0400

    path updates

--- js/includes.js
+++ js/includes.js
@@ -1,21 +1,19 @@
-const path = location.origin + '/includes/';
-
-fetch(path + "header.html").then(res => res.text()).then(data => {
+fetch("/includes/header.html").then(res => res.text()).then(data => {
     document.querySelector("header").innerHTML = data;
 });
 
 if (!document.querySelector(".return")) {
-    fetch(path + "nav.html").then(res => res.text()).then(data => {
+    fetch("/includes/nav.html").then(res => res.text()).then(data => {
         document.querySelector("nav").innerHTML = data;
     });
 }
 
 if (!document.querySelector(".footer-support")) {
-    fetch(path + "footer.html").then(res => res.text()).then(data => {
+    fetch("/includes/footer.html").then(res => res.text()).then(data => {
         document.querySelector("footer").innerHTML = data;
     });
 } else {
-    fetch(path + "footer-support.html").then(res => res.text()).then(data => {
+    fetch("/includes/footer-support.html").then(res => res.text()).then(data => {
         document.querySelector(".footer-support").innerHTML = data;
     });
 }
